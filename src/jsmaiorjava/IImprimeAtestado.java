70cfc4632437e8bb6c985441a12b6069203c90ce
MrDallOca <tdalloca@gmail.com>
2019-05-14 12:03:22 -0300

    Adicionado atestado

---
+++ src/jsmaiorjava/IImprimeAtestado.java
@@ -0,0 +1,9 @@
+package jsmaiorjava;
+
+interface IImprimeAtestado {
+    /*
+    Gera uma imagem de um atestado o diagnostico da doenca e a rubrica do doutor
+    */
+    boolean imprime(IDiagnostico diagnostico);
+}
+
