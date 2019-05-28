6ae27caaf1099ff5343f818720b6632e7def6566
Victor Coelho <victor09120@gmail.com>
2019-05-28 11:05:15 -0300

    Modificações no ImprimeAtestado

--- src/main/java/jsmaiorjava/ImprimeAtestado.java
+++ src/main/java/jsmaiorjava/ImprimeAtestado.java
@@ -2,21 +2,10 @@ package jsmaiorjava;
 import java.util.ArrayList;
 
 public class ImprimeAtestado implements IImprimeAtestado {
-    ITratamento tratamento;
-    public ImprimeAtestado(IDataSet dataset) {
-        this.tratamento = new Tratamento(dataset);
+    public ImprimeAtestado() {
     }
 
-    public void imprime(String doutor, String paciente, String doenca) {
-        String trt;
-        int trt_index = this.tratamento.getTratamento().indexOf(doenca);
-
-        if(trt_index != -1) {
-            trt = this.tratamento.getTratamento().get(trt_index + 1);
-        } else {
-            trt = "não existe tratamento";
-        }
-
-        System.out.println("Nome do doutor: " + doutor + " Nome do Paciente: " + paciente + "\n" + "Tratamento: " + trt);
+    public void imprime(IProntuario prontuario) {
+        System.out.println("Nome do doutor: " + prontuario.doutor() + " Nome do Paciente: " + prontuario.paciente() + "\n" + "Tratamento: " + prontuario.tratamento());
     }
 }
