ee1607a4d90c48b4c8964762e78b8a6f7819dcda
Gabriel <gabrielferreira241196@gmail.com>
2019-06-04 12:04:52 -0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/jsmaiorjava/Main.java
+++ src/main/java/jsmaiorjava/Main.java
@@ -3,13 +3,7 @@ import jsmaiorjava.interfaces.*;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Window win = new Window();
         win.open();
-=======
-    }
-
->>>>>>> master
-
     }
 }
--- src/main/java/jsmaiorjava/interfaces/IWindow.java
+++ src/main/java/jsmaiorjava/interfaces/IWindow.java
@@ -2,4 +2,6 @@ package jsmaiorjava.interfaces;
 
 public interface IWindow {
     void open();
+    void setLabelOne(String name, Boolean visible);
+    void setButtonName(String name, Boolean visible);
 }
